885cb6df2cecff69f9f72b73c374d9e6b047d539
861793462 <861793462@qq.com>
2020-10-19 19:37:12 +0800

    第一次提交

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    content:"world ! I'm comming",
+    movies:['大话西游','盗梦空间','星际穿越','少年派'],
+    counter:0,
+  },
+  onChangeTab(){
+    this.setData({
+      content:"kobe"
+    })
+  },
+  increment(){
+    this.setData({
+      counter:++this.data.counter
+    })
+  },
+  decrement(){
+    this.setData({
+      counter:--this.data.counter
+    })
   },
 
   /**
